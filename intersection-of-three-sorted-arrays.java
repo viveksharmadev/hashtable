746884c7a98a4c85247afcb5e1339376b340ab73
Learner <vivek.gurukul1982@gmail.com>
2019-11-06 11:53:24 -0800

    Update intersection-of-three-sorted-arrays.java

--- intersection-of-three-sorted-arrays.java
+++ intersection-of-three-sorted-arrays.java
@@ -45,3 +45,24 @@ class intersection-of-three-sorted-arrays {
         return res;
     }
 }
+
+class intersection-of-three-sorted-arrays {
+    // tc -> n, sc-> 1
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        int i=0, j=0, k=0;
+        List<Integer> res = new LinkedList<>();
+        while(i<arr1.length && j<arr2.length && k<arr3.length){
+            if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+                res.add(arr1[i]);
+                i++; j++; k++;
+            }else if(arr1[i]<arr2[j]){
+                i++;
+            }else if(arr2[j]<arr3[k]){
+                j++;
+            }else{
+                k++;
+            }
+        }
+        return res;
+    }
+}
